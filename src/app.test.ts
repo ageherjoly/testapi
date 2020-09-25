2e0870ba10f799db3789e314c84e97bcbe115b41
aurelien.geher <aurelien.geher@sysnav.fr>
2020-09-25 23:10:03 +0200

    [*] Add test app skeleton

---
+++ src/app.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest'
+import app from './app'
+
+describe('API form Request', () => {
+
+  // test('should return form on call', async () => {
+  //   const response = await request(app).get("/form")
+  //   expect(response.text).toBe('')
+  // })
+
+})
